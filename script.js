3ff5323331eff018778b0904cd9b5f77e7d8739e
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-30 12:05:43 +0200

    feat: done, added counting via keyevents space/enter

--- script.js
+++ script.js
@@ -5,9 +5,26 @@ let count = 0;
 
 main.onclick = function () {
   count += 1;
+  let size = count + "%";
   document.querySelector("#num").innerText = count;
+  main.style.backgroundSize = size;
 };
 btn.onclick = function () {
   document.querySelector("#num").innerText = 0;
   count = 0;
+  main.style.backgroundSize = 0;
 };
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    count += 1;
+    let size = count + "%";
+    document.querySelector("#num").innerText = count;
+    main.style.backgroundSize = size;
+  } else if (event.code === "Enter") {
+    count += 1;
+    let size = count + "%";
+    document.querySelector("#num").innerText = count;
+    main.style.backgroundSize = size;
+  }
+});
